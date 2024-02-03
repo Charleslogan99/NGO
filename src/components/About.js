bfb6ec0efadfead9be63243d9b4b9c7c7c7c4b2e
Charly <user@users-Air.lan>
2024-02-04 00:56:17 +0100

    card update

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,14 @@ const links = [
     { name: 'About Us', href: '' },
     { name: 'Our Major Causes', href: '' },
     { name: 'Our values', href: '' },
-    { name: 'Meet our leadership', href: '#' },
+    { name: 'Meet our leadership', href: '' },
   ]
-  const stats = [
-    { name: 'Offices worldwide', value: '12' },
-    { name: 'Full-time colleagues', value: '300+' },
-    { name: 'Hours per week', value: '40' },
-    { name: 'Paid time off', value: 'Unlimited' },
-  ]
+  // const stats = [
+  //   { name: 'Offices worldwide', value: '12' },
+  //   { name: 'Full-time colleagues', value: '20+' },
+  //   { name: 'Hours per week', value: '40' },
+  //   { name: 'Paid time off', value: 'Unlimited' },
+  // ]
   
   export default function About() {
     return (
@@ -20,7 +20,7 @@ const links = [
           <img
             src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
             alt=""
-            className="absolute inset-0 -z-10  h-full w-full object-cover object-right md:object-center"
+            className="absolute inset-0 -z-10   h-full w-full object-cover object-right md:object-center"
           />
           <div
             className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
@@ -51,11 +51,7 @@ const links = [
               <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
                 ABOUT US
               </h2>
-              {/* <p className="mt-6 text-lg leading-8 text-gray-300">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
-              </p> */}
+              
             </div>
             <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
               <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
@@ -70,7 +66,7 @@ const links = [
                 ))}
               </div>
               <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-                {stats.map((stat) => (
+                {/* {stats.map((stat) => (
                   <div key={stat.name} className="flex flex-col-reverse">
                     <dt className="text-base leading-7 text-gray-300">
                       {stat.name}
@@ -79,7 +75,7 @@ const links = [
                       {stat.value}
                     </dd>
                   </div>
-                ))}
+                ))} */}
               </dl>
             </div>
           </div>
--- src/components/Features.js
+++ src/components/Features.js
@@ -59,54 +59,133 @@
 
 // export default Features;
 
-import { FaGraduationCap, FaMedkit, FaUtensils, FaTshirt } from 'react-icons/fa';
+// import { FaGraduationCap, FaMedkit, FaUtensils, FaTshirt } from 'react-icons/fa';
+
+// const Features = () => {
+//   return (
+//     <div className="container mx-auto pb-16 px-4 py-8">
+//       <div className='p-8'>
+//         <h1 className='font-semibold text-4xl text-purple-700 text-center'>Our Major Causes</h1>
+//         <p className='text-sm pt-4 text-black text-center'>Help people out of poverty, and make their lives better</p>
+//       </div>
+//       <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
+//         {/* Card 1 */}
+//         <div className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
+//           <FaGraduationCap className="w-full h-64 text-purple-700 p-16" />
+//           <div className="p-4">
+//             <h3 className="text-xl font-semibold mb-2">Education for every child</h3>
+//             <p className="text-purple-700">Card 1 Description</p>
+//           </div>
+//         </div>
+
+//         {/* Card 2 */}
+//         <div className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
+//           <FaMedkit className="w-full h-64 text-purple-700 p-16" />
+//           <div className="p-4">
+//             <h3 className="text-xl font-semibold mb-2">Providing medical care</h3>
+//             <p className="text-purple-700">Card 2 Description</p>
+//           </div>
+//         </div>
+
+//         {/* Card 3 */}
+//         <div className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
+//           <FaUtensils className="w-full h-64 text-purple-700 p-16" />
+//           <div className="p-4">
+//             <h3 className="text-xl font-semibold mb-2">Feeding the hungry people</h3>
+//             <p className="text-purple-700">Card 3 Description</p>
+//           </div>
+//         </div>
+
+//         {/* Card 4 */}
+//         <div className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
+//           <FaTshirt className="w-full h-64 text-purple-700 p-16" />
+//           <div className="p-4">
+//             <h3 className="text-xl font-semibold mb-2">Providing clothing for people</h3>
+//             <p className="text-purple-700">Card 4 Description</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Features;
+
+import React from 'react';
+import { FaGraduationCap, FaMedkit, FaUtensils, FaHome, FaHandshake, FaMoneyBillAlt, FaBalanceScale, FaUsers } from 'react-icons/fa';
 
 const Features = () => {
-  return (
-    <div className="container mx-auto pb-16 px-4 py-8">
-      <div className='p-8'>
-        <h1 className='font-semibold text-4xl text-purple-700 text-center'>Our Major Causes</h1>
-        <p className='text-sm pt-4 text-black text-center'>Help people out of poverty, and make their lives better</p>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
-        {/* Card 1 */}
-        <div className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
-          <FaGraduationCap className="w-full h-64 text-purple-700 p-16" />
-          <div className="p-4">
-            <h3 className="text-xl font-semibold mb-2">Education for every child</h3>
-            <p className="text-purple-700">Card 1 Description</p>
-          </div>
-        </div>
+    // Define the causes and their respective icons
+    const causes = [
+      {
+        title: "Education",
+        icon: <FaGraduationCap />,
+        description:
+          "Providing quality education for every child ensures they have the opportunity to learn, grow, and reach their full potential. Education empowers children to break the cycle of poverty, equips them with essential skills for the future, and fosters a more inclusive society.",
+      },
+      {
+        title: "Healthcare",
+        icon: <FaMedkit />,
+        description:
+          "Access to adequate medical care is essential for maintaining good health and well-being. By providing medical care, we ensure that individuals, especially widows and their families, receive timely treatment, preventive care, and support services to address their healthcare needs and improve their quality of life.",
+      },
+      {
+        title: "Food Assistance",
+        icon: <FaUtensils />,
+        description:
+          "Hunger is a pressing issue affecting millions of people worldwide, including widows and their families. By providing nutritious food and meals, we address immediate hunger needs, alleviate food insecurity, and promote healthy living. Our goal is to ensure that no one goes to bed hungry and that everyone has access to nourishing food.",
+      },
+      {
+        title: "Shelter",
+        icon: <FaHome />,
+        description:
+          "Shelter is a basic human need and a fundamental aspect of security and well-being. By providing safe and stable housing, we offer individuals and families a sense of stability, protection, and dignity. Our goal is to ensure that everyone has access to secure and affordable housing options, regardless of their circumstances.",
+      },
+      {
+        title: "Empowerment",
+        icon: <FaHandshake />,
+        description:
+          "Empowerment initiatives equip individuals with the knowledge, skills, and resources they need to advocate for themselves and effect positive change in their lives and communities. By fostering empowerment, we enable individuals to overcome challenges, pursue opportunities, and realize their full potential.",
+      },
+      {
+        title: "Financial Support",
+        icon: <FaMoneyBillAlt />,
+        description:
+          "Financial support programs provide individuals with the means to meet their basic needs, pursue education and employment opportunities, and build financial stability. By offering financial assistance, we empower individuals to achieve financial independence, reduce economic disparities, and create brighter futures for themselves and their families.",
+      },
+      {
+        title: "Legal Aid",
+        icon: <FaBalanceScale />,
+        description:
+          "Legal aid services ensure that individuals have access to justice and legal representation, regardless of their socioeconomic status. By providing legal assistance, we protect individuals rights, promote fairness and equality under the law, and uphold the principles of justice and human rights.",
+      },
+      {
+        title: "Community Building",
+        icon: <FaUsers />,
+        description:
+          "Community building initiatives foster social connections, collaboration, and mutual support among individuals and groups within communities. By strengthening social networks and promoting inclusivity, we create vibrant and resilient communities where everyone feels valued, respected, and empowered to contribute positively to society.",
+      },
+    ];
 
-        {/* Card 2 */}
-        <div className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
-          <FaMedkit className="w-full h-64 text-purple-700 p-16" />
-          <div className="p-4">
-            <h3 className="text-xl font-semibold mb-2">Providing medical care</h3>
-            <p className="text-purple-700">Card 2 Description</p>
-          </div>
+    return (
+        <div className="container mx-auto px-4 py-8 mb-20">
+            <h1 className='text-center my-8 text-6xl text-purple-700 font-satoshi mb-10'>Our Major Causes</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 ">
+                {/* Map through causes and render dynamic cards */}
+                {causes.map((cause, index) => (
+                    <div key={index} className="bg-white transition duration-300 ease-in-out transform hover:scale-105 rounded-lg shadow-md p-4 flex flex-col items-center">
+                        {/* Icon */}
+                        <div className="  text-purple-700 mb-2">
+                            <span className="text-5xl">{cause.icon}</span>
+                        </div>
+                        {/* Text */}
+                        <p className="text-2xl mb-4 text-center font-semibold text-gray-800">{cause.title}</p>
+                        <p className='text-sm text-center'>{cause.description}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-
-        {/* Card 3 */}
-        <div className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
-          <FaUtensils className="w-full h-64 text-purple-700 p-16" />
-          <div className="p-4">
-            <h3 className="text-xl font-semibold mb-2">Feeding the hungry people</h3>
-            <p className="text-purple-700">Card 3 Description</p>
-          </div>
-        </div>
-
-        {/* Card 4 */}
-        <div className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
-          <FaTshirt className="w-full h-64 text-purple-700 p-16" />
-          <div className="p-4">
-            <h3 className="text-xl font-semibold mb-2">Providing clothing for people</h3>
-            <p className="text-purple-700">Card 4 Description</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Features;
