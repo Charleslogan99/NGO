15fb1a80864806177c97c062a26d97c1a33f360d
Charly <user@users-MacBook-Air.local>
2024-02-26 16:20:48 +0100

    support page update

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,7 +5,7 @@ import 'slick-carousel/slick/slick-theme.css';
 
 const Carousel = ({ title, date, description, images }) => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -19,7 +19,7 @@ const Carousel = ({ title, date, description, images }) => {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: false
         }
       },
       {
